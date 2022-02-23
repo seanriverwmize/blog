6f50cf08158f517b65ba486bee2710b8c5eee16e
Sean Mize <seanriverwmize@gmail.com>
2022-02-23 01:08:50 -0600

    finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About">
+      <p>
+        I write code or whatever. This is a blog I made to share some of my projects and my thoughts about programming.
+        <br></br>
+        -Sean Mize
+      </p>
+    </Layout>
+  )
+}
+
+export default AboutPage
